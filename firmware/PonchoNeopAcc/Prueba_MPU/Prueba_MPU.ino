988f407addf17dc4549e7000483b7fdc4884d679
Diego Brengi <diego.brengi@gmail.com>
2018-09-29 15:22:00 -0300

    Agrego ejemlos del Shield de Neopixel+Relé+Acelerómetros, verifiados por Spinazola.

---
+++ firmware/PonchoNeopAcc/Prueba_MPU/Prueba_MPU.ino
@@ -0,0 +1,68 @@
+#include "I2Cdev.h"
+#include "MPU6050.h"
+
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+MPU6050 accelgyro;
+
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+#define OUTPUT_READABLE_ACCELGYRO
+
+#define LED_PIN 13
+bool blinkState = false;
+
+void setup() {
+
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    Serial.begin(38400);
+
+    Serial.println("Initializing I2C devices...");
+    accelgyro.initialize();
+
+    Serial.println("Testing device connections...");
+    Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+    
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+
+    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+    #ifdef OUTPUT_READABLE_ACCELGYRO
+
+        Serial.print("a/g:\t");
+        Serial.print(ax); Serial.print("\t");
+        Serial.print(ay); Serial.print("\t");
+        Serial.print(az); Serial.print("\t");
+        Serial.print(gx); Serial.print("\t");
+        Serial.print(gy); Serial.print("\t");
+        Serial.println(gz);
+        delay(200);
+    #endif
+
+    #ifdef OUTPUT_BINARY_ACCELGYRO
+        Serial.write((uint8_t)(ax >> 8)); Serial.write((uint8_t)(ax & 0xFF));
+        Serial.write((uint8_t)(ay >> 8)); Serial.write((uint8_t)(ay & 0xFF));
+        Serial.write((uint8_t)(az >> 8)); Serial.write((uint8_t)(az & 0xFF));
+        Serial.write((uint8_t)(gx >> 8)); Serial.write((uint8_t)(gx & 0xFF));
+        Serial.write((uint8_t)(gy >> 8)); Serial.write((uint8_t)(gy & 0xFF));
+        Serial.write((uint8_t)(gz >> 8)); Serial.write((uint8_t)(gz & 0xFF));
+        delay(200);
+    #endif
+
+    blinkState = !blinkState;
+    digitalWrite(LED_PIN, blinkState);
+
+    
+}
---
+++ firmware/PonchoNeopAcc/Prueba_rele_y_neopixel/Prueba_rele_y_neopixel.ino
@@ -0,0 +1,43 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+#define PIN            6
+
+#define NUMPIXELS      8
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+int delayval = 500; // delay for half a second
+
+void setup() {
+ 
+#if defined (__AVR_ATtiny85__)
+  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+#endif
+
+  pixels.begin(); 
+}
+
+void loop() {
+
+  for(int i=0;i<NUMPIXELS;i++){
+    pixels.setPixelColor(i, pixels.Color(100,0,0)); 
+    pixels.setPixelColor(i-1, pixels.Color(0,100,0));
+    pixels.setPixelColor(i-2, pixels.Color(0,0,100));
+    pixels.show(); 
+    delay(200); 
+    pixels.setPixelColor(i-1, pixels.Color(0,0,0));
+    pixels.setPixelColor(i-2, pixels.Color(0,0,0));
+    pixels.setPixelColor(i, pixels.Color(0,0,0));
+    pixels.show();
+
+    if(i>=0 && i<4){
+      digitalWrite(7,HIGH);
+    }
+    else{
+      digitalWrite(7,LOW);
+    }
+   }
+}
